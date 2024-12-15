58aa749d5b7fd8d5b54bdb19028d7ac673eaa384
Chosamee <cytor@naver.com>
2024-12-16 02:35:22 +0900

    get cookie test code

--- src/main/java/hello/hello_spring/conroller/HelloController.java
+++ src/main/java/hello/hello_spring/conroller/HelloController.java
@@ -4,12 +4,15 @@ import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import java.util.UUID;
 
 @Controller
 public class HelloController {
@@ -19,8 +22,18 @@ public class HelloController {
         return "hello";
     }
 
+    @GetMapping("cookie")
+    public ResponseEntity<Void> setCookies(@RequestParam("kafkaName") String kafkaName) {
+        String clientId = UUID.randomUUID().toString();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Set-Cookie", "kafkaName=" + kafkaName + "; Path=/; HttpOnly");
+        headers.add("Set-Cookie", "clientId=" + clientId + "; Path=/; HttpOnly");
+        return ResponseEntity.ok().headers(headers).build();
+    }
+
     @GetMapping("sse")
-    public SseEmitter getMethodName(@CookieValue String clientId) {
+    public SseEmitter getMethodName(@CookieValue("clientId") String clientId,
+            @CookieValue("kafkaName") String kafkaName) {
         SseEmitter emitter = new SseEmitter();
         ExecutorService executor = Executors.newSingleThreadExecutor();
         emitter.onCompletion(() -> {
@@ -42,7 +55,7 @@ public class HelloController {
                 for (int i = 0; i < 10; i++) {
                     emitter.send(SseEmitter.event()
                             .name("message")
-                            .data("Hello " + clientId + ", message #" + (i + 1)));
+                            .data("Hello " + clientId + ", message #" + (i + 1) + " from " + kafkaName));
                     Thread.sleep(1000);
                 }
                 emitter.complete();
